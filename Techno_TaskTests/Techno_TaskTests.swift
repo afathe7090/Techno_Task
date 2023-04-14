21d0816d7ab402663bbba5c10a54d3ad23067255
Ahmed Fathy <afathe7090@gmail.com>
2023-04-14 15:25:21 +0200

    adding testes to app and remove pods from configuration File

---
+++ Techno_TaskTests/Techno_TaskTests.swift
@@ -0,0 +1,16 @@
+//
+//  Techno_TaskTests.swift
+//  Techno_TaskTests
+//
+//  Created by Ahmed Fathy on 14/04/2023.
+//
+
+import XCTest
+@testable import Techno_Task
+final class Techno_TaskTests: XCTestCase {
+    
+    func test_iSCircleCi_WorkingWithTests() {
+        let x = 1
+        XCTAssertNotNil(x)
+    }
+}
